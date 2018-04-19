23270ed86e9675f3e8b3ddcf4daa0e5bac1e8556
kyodgorbek <kyodgorbek@gmail.com>
2018-04-19 21:01:42 +0200

    I have added some json array fields

--- app/src/main/java/com/example/yodgorbekkomilov/footballlovers/Pojo/Match.java
+++ app/src/main/java/com/example/yodgorbekkomilov/footballlovers/Pojo/Match.java
@@ -20,6 +20,36 @@ public class Match {
     @SerializedName("time")
     @Expose
     private String time;
+    @SerializedName("league_id")
+    @Expose
+    private String leagueId;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("added")
+    @Expose
+    private String added;
+
+
+    @SerializedName("last_changed")
+    @Expose
+    private String lastChanged;
+    @SerializedName("home_id")
+    @Expose
+    private String homeId;
+    @SerializedName("away_id")
+    @Expose
+    private String awayId;
+
+    @SerializedName("events")
+    @Expose
+    private String events;
+
+
+    @SerializedName("league_name")
+    @Expose
+    private String leagueName;
+
 
     public String getId() {
         return id;
@@ -61,4 +91,67 @@ public class Match {
         this.time = time;
     }
 
+    public String getLeagueId() {
+        return leagueId;
+    }
+
+    public void setLeagueId(String leagueId) {
+        this.leagueId = leagueId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAdded() {
+        return added;
+    }
+
+    public void setAdded(String added) {
+        this.added = added;
+    }
+
+    public String getLastChanged() {
+        return lastChanged;
+    }
+
+    public void setLastChanged(String lastChanged) {
+        this.lastChanged = lastChanged;
+    }
+
+    public String getHomeId() {
+        return homeId;
+    }
+
+    public void setHomeId(String homeId) {
+        this.homeId = homeId;
+    }
+
+    public String getAwayId() {
+        return awayId;
+    }
+
+    public void setAwayId(String awayId) {
+        this.awayId = awayId;
+    }
+
+    public String getEvents() {
+        return events;
+    }
+
+    public void setEvents(String events) {
+        this.events = events;
+    }
+
+    public String getLeagueName() {
+        return leagueName;
+    }
+
+    public void setLeagueName(String leagueName) {
+        this.leagueName = leagueName;
+    }
 }
--- app/src/main/java/com/example/yodgorbekkomilov/footballlovers/Pojo/Response.java
+++ app/src/main/java/com/example/yodgorbekkomilov/footballlovers/Pojo/Response.java
@@ -3,7 +3,7 @@ package com.example.yodgorbekkomilov.footballlovers.Pojo;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Example {
+public class Response {
 
     @SerializedName("success")
     @Expose
